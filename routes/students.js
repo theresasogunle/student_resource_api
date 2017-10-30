1adbc325d3635e3007761e39316f84d9727e3f3e
Tessa <sogunletheresa@gmail.com>
2017-10-30 02:25:37 +0100

    Initial commit

--- routes/students.js
+++ routes/students.js
@@ -1,8 +1,8 @@
 var mongo = require('mongodb');
 
-var Server = mongo.Server,
-    Db = mongo.Db,
-    BSON = mongo.BSONPure;
+//var Server = mongo.Server,
+  //  Db = mongo.Db,
+    //BSON = mongo.BSONPure;
 
 //var server = new Server('localhost', 27017, {auto_reconnect: true});
 var db = mongo.db(process.env.MONGOLAB_URI, {native_parser:true});
