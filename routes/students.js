a44ce6ad5873f6dbe945d35a27b30cc76128364a
Tessa <sogunletheresa@gmail.com>
2017-11-03 15:07:09 +0100

    Initial commit

--- routes/students.js
+++ routes/students.js
@@ -1,23 +1,13 @@
 var mongo = require('mongodb');
 
-// Connect to the database before starting the application server.
+
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://root:root@ds123410.mlab.com:23410/heroku_2snqp2d4');
 
 
-
-
-
-// our db model
 var Student = require("../models/model.js");
 
-/**
- * GET '/'
- * Default home route. Just relays a success message back.
- * @param  {Object} req
- * @return {Object} json
- */
 
 exports.index = function(req, res) {
     
@@ -28,23 +18,18 @@ exports.index = function(req, res) {
         message: 'Welcome to the STUDENT RESOURCES API'
     }
 
-    // respond back with the data
+  
     res.json(data);
 
 }
 
-/**
- * POST '/api/create'
- * Receives a POST request of the new user and location, saves to db, responds back
- * @param  {Object} req. An object containing the different attributes of the Person
- * @return {Object} JSON
- */
+
 
 exports.create = function(req,res){
 
     console.log(req.body);
 
-    // pull out the name and location
+   
     var surname = req.body.surname;
     var firstname = req.body.firstname;
     var matric_number = req.body.matric_number;
@@ -59,11 +44,9 @@ exports.create = function(req,res){
         
       });
 
-      // now, save that person to the database
-        // mongoose method, see http://mongoosejs.com/docs/api.html#model_Model-save      
+       
       student.save(function(err,data){
-        // if err saving, respond back with error
-        if (err){
+       
             var jsonData = {status:'ERROR', message: 'Error saving student'};
             return res.json(jsonData);
         }
@@ -71,7 +54,7 @@ exports.create = function(req,res){
         console.log('saved a new student!');
         console.log(data);
 
-        // now return the json data of the new person
+       
         var jsonData = {
             status: 'OK',
             student: data
@@ -83,27 +66,21 @@ exports.create = function(req,res){
  
 }
 
-/**
- * GET '/api/get/:id'
- * Receives a GET request specifying the user to get
- * @param  {String} req.param('id'). The userId
- * @return {Object} JSON
- */
+
 
 exports.getOne = function(req,res){
 
     var requestedId = req.param('id');
 
-    // mongoose method, see http://mongoosejs.com/docs/api.html#model_Model.findById
+    
     Student.findById(requestedId, function(err,data){
 
-        // if err or no user found, respond with error 
+      
         if(err || data == null){
-        var jsonData = {status:'ERROR', message: 'Could not find that person'};
+        var jsonData = {status:'ERROR', message: 'Could not find that student'};
          return res.json(jsonData);
     }
 
-    // otherwise respond with JSON data of the user
     var jsonData = {
         status: 'OK',
         student: data
@@ -114,23 +91,17 @@ exports.getOne = function(req,res){
     })
 }
 
-/**
- * GET '/api/get'
- * Receives a GET request to get all user details
- * @return {Object} JSON
- */
+
 
 exports.getAll = function(req,res){
 
-    // mongoose method, see http://mongoosejs.com/docs/api.html#model_Model.find
     Student.find(function(err, data){
-        // if err or no users found, respond with error 
-        if(err || data == null){
-        var jsonData = {status:'ERROR', message: 'Could not find people'};
+    if(err || data == null){
+        var jsonData = {status:'ERROR', message: 'Could not find students'};
         return res.json(jsonData);
     }
 
-    // otherwise, respond with the data 
+  
 
     var jsonData = {
         status: 'OK',
@@ -143,20 +114,13 @@ exports.getAll = function(req,res){
 
 }
 
-/**
- * POST '/api/update/:id'
- * Receives a POST request with data of the user to update, updates db, responds back
- * @param  {String} req.param('id'). The userId to update
- * @param  {Object} req. An object containing the different attributes of the Person
- * @return {Object} JSON
- */
+
 
 exports.update = function(req,res){
 
     var requestedId = req.param('id');
 
-    // pull out the name and location
-    
+   
     var surname = req.body.surname;
     var firstname = req.body.firstname;
     var matric_number = req.body.matric_number;
@@ -173,10 +137,9 @@ exports.update = function(req,res){
         
       };
 
-      // now, update that person
-        // mongoose method, see http://mongoosejs.com/docs/api.html#model_Model.findByIdAndUpdate  
+     
       Student.findByIdAndUpdate(requestedId, dataToUpdate, function(err,data){
-        // if err saving, respond back with error
+       
         if (err){
             var jsonData = {status:'ERROR', message: 'Error updating student data'};
             return res.json(jsonData);
@@ -185,7 +148,7 @@ exports.update = function(req,res){
         console.log('updated the student data!');
         console.log(data);
 
-        // now return the json data of the new person
+       
         var jsonData = {
             status: 'OK',
             student: data
@@ -198,25 +161,20 @@ exports.update = function(req,res){
    
 }
 
-/**
- * GET '/api/delete/:id'
- * Receives a GET request specifying the user to delete
- * @param  {String} req.param('id'). The userId
- * @return {Object} JSON
- */
+
 
 exports.remove = function(req,res){
 
     var requestedId = req.param('id');
 
-    // Mongoose method, http://mongoosejs.com/docs/api.html#model_Model.findByIdAndRemove
+   
     Student.findByIdAndRemove(requestedId,function(err, data){
         if(err || data == null){
         var jsonData = {status:'ERROR', message: 'Could not find that student to delete'};
         return res.json(jsonData);
         }
 
-        // otherwise, respond back with success
+        
         var jsonData = {
             status: 'OK',
             message: 'Successfully deleted id ' + requestedId
--- server.js
+++ server.js
@@ -9,12 +9,10 @@ var path = require('path');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
-// the ExpressJS App
 var app = express();
 
-// configuration of expressjs settings for the web server.
 
-// server port number
+
 app.set('port', process.env.PORT || 5000);
 
 app.use(bodyParser.json());
@@ -22,58 +20,43 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 
 
-/**
- * CORS support for AJAX requests
- */
-
 app.all('*', function(req, res, next){
   if (!req.get('Origin')) return next();
-  // use "*" here to accept any origin
+  
   res.set('Access-Control-Allow-Origin', '*');
   res.set('Access-Control-Allow-Methods', 'PUT');
   res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
-  // res.set('Access-Control-Allow-Max-Age', 3600);
   if ('OPTIONS' == req.method) return res.send(200);
   next();
 });
 
-// api baseURI is at /api/
 
-// API Routes 
-
-// CREATE - http://appname.com/api/create (POST)
-// RETRIEVE 1 - http://appname.com/api/get/:id (GET)
-// RETRIEVE ALL - http://appname.com/api/get (GET)
-// UPDATE - http://appname.com/api/update/:id (PUT)
-// DELETE - http://appname.com/api/delete/:id (DELETE)
-
-// ROUTES, logic is in routes/index.js
 
 var routes = require('./routes/students.js');
 
-// home route is not really an API route, but does respond back
-app.get('/', routes.index); // calls index function in /routes/index.js
 
-// API routes
-app.post('/api/create', routes.create); // API create route and callback (see /routes/index.js)
-app.get('/api/get/:id', routes.getOne); // API retrieve 1 route and callback (see /routes/index.js)
-app.get('/api/get', routes.getAll); // API retrieve all route and callback (see /routes/index.js)
-app.put('/api/update/:id', routes.update); // API update route and callback (see /routes/index.js)
-app.post('/api/delete/:id', routes.remove); // API delete route and callback (see /routes/index.js)
+app.get('/', routes.index); 
 
-// if route not found, respond with 404
+
+app.post('/api/create', routes.create); 
+app.get('/api/get/:id', routes.getOne); 
+app.get('/api/get', routes.getAll); 
+app.put('/api/update/:id', routes.update); 
+app.post('/api/delete/:id', routes.remove); 
+
+
 app.use(function(req, res, next){
 
 	var jsonData = {
 		status: 'ERROR',
 		message: 'Sorry, we cannot find the requested URI'
 	}
-	// set status as 404 and respond with data
+	
   res.status(404).send(jsonData);
 
 });
 
-// create NodeJS HTTP server using 'app'
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
