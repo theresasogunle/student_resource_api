6278f4aa737959bf9d17ea74c2ae9a6bdb224e02
Tessa <sogunletheresa@gmail.com>
2017-11-03 15:25:08 +0100

    Initial commit

--- models/model.js
+++ models/model.js
@@ -1,8 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-// See http://mongoosejs.com/docs/schematypes.html
-
 var studentSchema = new Schema({
 	surname: String,
 	firstname: String,
@@ -11,5 +9,4 @@ var studentSchema = new Schema({
 	dateAdded : { type: Date, default: Date.now },
 })
 
-// export 'Person' model so we can interact with it in other files
 module.exports = mongoose.model('Student',studentSchema);
